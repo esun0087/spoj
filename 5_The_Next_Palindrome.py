74dc568db977f27f4b2775145099fe991c9a3136
unknown <>
2016-06-12 07:13:38 +0800

    commit 5.py

--- 5_The_Next_Palindrome.py
+++ 5_The_Next_Palindrome.py
@@ -1,7 +1,7 @@
 t = int(input())
 
 def process_line(line):
-    if len(line) == 1 && line[0] < 9:
+    if len(line) == 1 and line[0] < 9:
         return  [line[0] + 1]
     elif len (line) == 1:
         return [11]
@@ -16,6 +16,7 @@ def process_line(line):
         if tmp > line:
             return tmp 
         cur = L // 2
+        m = L // 2
         tmp[cur] += 1
         if tmp[cur] > 9:
             c = 1
@@ -28,9 +29,12 @@ def process_line(line):
                 c = 1
             else:
                 c = 0
+            cur += 1
         if cur == L:
             tmp.append(1)
-        
+        L = len(tmp)
+        m = L // 2
+        return tmp[-1:-m-1:-1] + tmp[m:]
         
 for i in range(t):
     line = input()
@@ -38,4 +42,6 @@ for i in range(t):
     if int(line) == 10**len(line) - 1:
         line = str(10**len(line))
     line = [int(i) for i in line]
-    print (line)
+    line = process_line(line)
+    line = [str(i) for i in line]
+    print (''.join(line))
