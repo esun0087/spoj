1ea7841b9e119643072ce742bf948a2f4d5c68f3
Biao Wang(王彪1) <wangbiao1@letv.com>
2016-06-14 12:46:42 +0800

    6 complete

--- 6_Simple_Arithmetics.py
+++ 6_Simple_Arithmetics.py
@@ -25,7 +25,7 @@ def process_add(a, b):
     else:
         ans[min_l] = add_data
     ans = ans[::-1]
-    max_len = L+1
+    max_len = len(b)+1
     ans_pos = 0
     for i, k in enumerate(ans):
         if k != 0:
@@ -67,7 +67,7 @@ def process_reduce(a, b):
         x = t - 0
         ans[i] = x
     ans = ans[::-1]
-    max_len = L+1
+    max_len = len(b)+1
     ans_pos = 0
     for i, k in enumerate(ans):
         if k != 0:
@@ -78,10 +78,11 @@ def process_reduce(a, b):
     ans = [str(i) for i in ans]
     a = ''.join([str(i) for i in a[::-1]])
     b = ''.join([str(i) for i in b[::-1]])
-    print (' '*(max_len - len(a))+a)
-    print (' '*(max_len - len(b) - 1) + '-' + b)
-    print ('-' * (max_len))
-    print (' '* (max_len - (len(ans) - ans_pos)) + ''.join(ans[ans_pos:]))
+    all_max_len = max(len(a), len(b) + 1, max_len)
+    print (' '*(all_max_len - len(a))+a)
+    print (' '*(all_max_len - len(b) - 1) + '-' + b)
+    print (' '*(all_max_len - max_len) + '-' * (max_len))
+    print (' '* (all_max_len - (len(ans) - ans_pos)) + ''.join(ans[ans_pos:]))
     return ans[::-1]
 
 def help_add(a, b):
@@ -110,7 +111,6 @@ def help_add(a, b):
     ans = ans[::-1]
     return ans
 def process_mul(a, b):
-    
     a = a[::-1]
     b = b[::-1]
     L = max(len(a), len(b))
@@ -133,32 +133,43 @@ def process_mul(a, b):
         for i, tmp in enumerate(ans):
             final_ans = help_add(final_ans, tmp+[0] * i)
         ans.append(final_ans)
-    ans_pos = 0
-    max_len = max(len(a), len(b) + 1)
+    max_len = len(b) + 1
+    first_max_len = max_len
+    for i, k in enumerate(ans[0]):
+        if k != 0:
+            if len(ans[0]) - i >= max_len:
+                first_max_len = len(ans[0]) - i
+            break
+    
     for i, k in enumerate(ans[-1]):
         if k != 0:
             if len(ans[-1]) - i >= max_len:
                 max_len = len(ans[-1]) - i
-            ans_pos = i
+            second_max_len = len(ans[-1]) - i
             break
     a = ''.join([str(i) for i in a[::-1]])
     b = ''.join([str(i) for i in b[::-1]])
-    print (' '*(max_len - len(a))+a)
-    print (' '*(max_len - len(b) - 1) + '*' + b)
-    print ('-' * (max_len))
+    all_max_len = max(len(a), len(b) + 1, max_len)
+    print (' '*(all_max_len - len(a))+a)
+    print (' '*(all_max_len - len(b) - 1) + '*' + b)
+    print (' '*(all_max_len - first_max_len) + '-' * (first_max_len))
     tag = False
     for i, arr in enumerate(ans):
-        tmp = ''.join([str(i) for i in arr])
+        for j in range(len(arr)):
+            if arr[j] != 0:
+                arr = arr[j:]
+                break
+        tmp = ''.join([str(tt) for tt in arr])
         if i == len(ans) - 1:
             if tag:
-                print ('-' * (max_len))
-            print (' '* (max_len - (len(tmp) - ans_pos)) + ''.join(tmp[ans_pos:]))
+                print (' '* (all_max_len-second_max_len) + '-' * (second_max_len))
+            print (' '* (all_max_len - (len(tmp))) + ''.join(tmp))
         else:
             tag = True
             if tmp == '0' * len(tmp):
-                print (' '*(max_len - i-1)+'0')
+                print (' '*(all_max_len - i-1)+'0')
             else:
-                print (' '*(max_len - len(tmp)-i)+tmp)
+                print (' '*(all_max_len - len(tmp)-i)+tmp)
 
     return ans
 
